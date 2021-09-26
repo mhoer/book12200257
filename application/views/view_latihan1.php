f445888e2d98334d0e706d2294cbdfc953fda2ee
mhoer <mhoerudin82@gmail.com>
2021-09-26 14:30:18 +0700

    Upload tugas per2 dan modif file

---
+++ application/views/view_latihan1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+    Hallo kawan.. yik kita belajar Web Programming..!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
