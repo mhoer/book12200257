56c9128e9063d68d76c5c679d5b6400bc71f680f
mhoer <mhoerudin82@gmail.com>
2021-10-05 14:38:54 +0700

    Tugas per3 dan modif file

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Muhamad</li>
+		<li>Nama Belakang : Hoerudin</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li>Sindang Barang Pilar 1 Bogor Barat</li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Bogor</li>
+	</ul>
+
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Bersepeda</li>
+		<li>Belajar bahasa asing</li>
+        <li>Membaca komik</li>
+	</ul>
+</section>
