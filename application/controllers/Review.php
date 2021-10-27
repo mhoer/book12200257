19bb320eaec003fab68e3b3663a5db52d20d4185
mhoer <mhoerudin82@gmail.com>
2021-10-27 12:43:24 +0700

    Tugas per6 (baru revisi)

---
+++ application/controllers/Review.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Review extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Review_model');
+    }
+
+    public function index()
+    {
+        $this->form_validation->set_rules('nama', 'Nama Pembeli', 'required',[
+            'required' => 'Nama Pembeli harus diisi'
+        ]);
+        $this->form_validation->set_rules('nhp', 'Nomer HP', 'required', [
+            'required' => 'Nomer HP harus diisi'
+        ]);
+        if ($this->form_validation->run() == false) {
+            $data['merk'] = ['Nike', 'Adidas', 'Kickers', 'Eiger', 'Bucherri'];
+            $this->load->view('v_input', $data);
+    }       else {
+            $data = [
+                'nama' => $this->input->post('nama'),
+                'nhp' => $this->input->post('nhp'),
+                'merk' => $this->input->post('merk'),
+                'ukuran' => $this->input->post('ukuran'),
+                'harga' => $this->Review_model->proses($this->input->post('merk'))
+            ];
+            
+            $this->load->view('v_output', $data);
+        }    
+    }
+}
