68dbfeb685dfb8d181c0ddccbbb6a7755cc9fff3
mhoer <mhoerudin82@gmail.com>
2021-11-18 10:30:46 +0700

    Tugas per10 dan modif file

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -11,6 +11,14 @@ class ModelUser extends CI_Model
     public function getUserWhere($where = null)
     {return $this->db->get_where('user' , $where);}
 
+    public function cekUserAccess($where = null)
+    {
+    $this->db->select('*');
+    $this->db->from('access_menu');
+    $this->db->where($where);
+    return $this->db->get();
+    }
+
     public function getUserLimit()
     {
     $this->db->select('*' );
