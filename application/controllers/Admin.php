16aaf5d6f9fa5d89c450d6f28dc37eee5853540f
mhoer <mhoerudin82@gmail.com>
2021-11-24 17:11:55 +0700

    Revisi per 10 dan 11

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,13 +2,13 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
- public function __construct()
- {
- parent::__construct();
- cek_login();
- }
- public function index()
- {
+    public function __construct()
+    {
+        parent::__construct();
+        cek_login();
+    }
+    public function index()
+    {
     $data['judul'] = 'Dashboard';
     $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
     $data['anggota'] = $this->ModelUser->getUserLimit()->result_array();
